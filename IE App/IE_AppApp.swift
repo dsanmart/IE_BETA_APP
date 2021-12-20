65cae0b888af5c5b4054cb0791cd72b3b20155fd
dsanmart <diegosancarrion@gmail.com>
2021-12-20 02:10:42 +0100

    Initial Commit

---
+++ IE App/IE_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IE_AppApp.swift
+//  IE App
+//
+//  Created by Diego Sanmartin on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct IE_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
